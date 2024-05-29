e10464bc8b9b3f047d7f2b8f9477c237d3f04cb3
NuReach <nureach46@gmail.com>
2024-05-29 20:20:37 +0700

    change navbar sign in sign up btn

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -2,18 +2,27 @@ import Link from 'next/link'
 import React from 'react'
 import { Button } from './button'
 import { SquareMenu } from "lucide-react"
+import { auth, currentUser } from '@clerk/nextjs/server'
+import { UserButton } from '@clerk/nextjs'
 
-
-export default function Navbar() {
+export default async function Navbar() {
+  const isAuth = auth();
+  const user = await currentUser();
+ 
   return (
     <div className='p-3 border-b shadow flex justify-between items-center'>
         <Link className='flex gap-3 items-center' href={'/dashboard'}>
             <SquareMenu />
             <h1 className='font-bold text-xl'>E-Admin</h1>
         </Link>
-        <div className='flex space-x-3'>
-            <Button>Logout</Button>
-        </div>
+        {
+          isAuth && 
+          <div className='flex space-x-3 items-center'>
+            <UserButton />
+            <h1 className='font-bold capitalize'>{user?.username}</h1>
+          </div>
+
+        }
     </div>
   )
 }
